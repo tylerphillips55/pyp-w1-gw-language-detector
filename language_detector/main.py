98ea6108dc97712c0683f2d601aecb1ae07c2b7b
Tyler <phillipst55@gmail.com>
2016-05-18 21:49:15 +0000

    New solution

--- language_detector/main.py
+++ language_detector/main.py
@@ -8,16 +8,16 @@ import re
 
 
 def detect_language(text, languages):
-    regex = re.compile('[%s]' % re.escape(string.punctuation))
-    ls = regex.sub('',text).split()
-    word_counts = {}
-    freq = {}
-    for word in ls:
-        word_counts[word] = word_counts.get(word,0) + 1
-    for dict in languages:
-        count = 0
-        for key,value in word_counts.items():
-            if key in dict["common_words"]:
-                count += value
-                freq[dict["name"]] = count
-    return max(freq, key=freq.get)
+    text = re.compile('[%s]' % re.escape(string.punctuation)).sub('',text)      #remove punctuation in string
+    ls = text.split()                                                           #returns list of words in text
+    word_counts = {}                                                            #dict will hold {word:frequency in text}
+    total = {}                                                                  #dict will hold {language:total common words that appear}
+    for word in ls:                                                             #iterate over words from text
+        word_counts[word] = word_counts.get(word,0) + 1                         #return current count of word if exists or 0 if it has not beed added yet, then increment by 1
+    for d in languages:                                                         #iterate over dictionaries 
+        count = 0                                                               #count tallies total number of common word occurences for langauge
+        for key,value in word_counts.items():                                   #iterate over word:frequency pairs in dict of words in text
+            if key in d["common_words"]:                                        #if word is a common word, 
+                count += value                                                  #   increment count by word frequency in text and
+                total[d["name"]] = count                                        #   add or update {language:total commmon words}
+    return max(total, key=total.get)                                            #return language with greatest common word total
